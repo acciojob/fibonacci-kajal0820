c6b818c701a5e9feb9969a2234f90d79b0d1b8a1
kajal0820 <130426994+kajal0820@users.noreply.github.com>
2025-06-06 09:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function fibonacci(num) {
+	 if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, num):
+        a, b = b, a + b
+    return b
 // your code here
 }
 
